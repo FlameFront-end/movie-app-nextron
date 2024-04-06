95b60bab577d64983e04955c2b9dc8dedaed6f02
Flame <5017_30@mail.ru>
2024-04-06 23:02:44 +0300

    fix

--- renderer/components/ui/PageHeader/PageHeader.tsx
+++ renderer/components/ui/PageHeader/PageHeader.tsx
@@ -1,16 +1,14 @@
-// import bg from '../../../../../../../../Users/5017_/OneDrive/Рабочий стол/test-nextron-movie/renderer/assets/footer-bg.jpg'
 import { FC, ReactNode } from 'react'
+import bg from '../../../assets/footer-bg.jpg'
 import s from './PageHeader.module.scss'
 
 interface PageHeaderProps {
 	children: ReactNode
 }
 
-// style={{ backgroundImage: `url(${bg})` }}
-
 const PageHeader: FC<PageHeaderProps> = ({ children }) => {
 	return (
-		<div className={s.pageHeader}>
+		<div className={s.pageHeader} style={{ backgroundImage: `url(${bg})` }}>
 			<h2>{children}</h2>
 		</div>
 	)
--- renderer/pages/movies/index.tsx
+++ renderer/pages/movies/index.tsx
@@ -1,14 +1,39 @@
 import { NextPage } from 'next'
+import { useState } from 'react'
+import Input from '../../components/Form/Input/Input'
+import MovieList from '../../components/Movie/MovieList/MovieList'
+import Button from '../../components/ui/Button/Button'
 import PageHeader from '../../components/ui/PageHeader/PageHeader'
 import Curve from '../../layouts/Curve'
+import s from './Movies.module.scss'
 
 const MoviesPage: NextPage = () => {
+	const [keyword, setKeyword] = useState('')
+
+	const onHandleSearch = () => {
+		console.log('onHandleSearch')
+	}
+
 	return (
 		<Curve>
 			<div className='wrapper'>
-				<PageHeader>Movies</PageHeader>
+				<PageHeader>Фильмы</PageHeader>
 				<div className='container'>
-					<div className='section mb-3'>MovieGrid</div>
+					<div className='section mb-3'>
+						<div className={s.movie_search}>
+							<Input
+								type='text'
+								placeholder='Введите название'
+								value={keyword}
+								onChange={e => setKeyword(e.target.value)}
+								name='keyword'
+							/>
+							<Button className={s.btn} onClick={onHandleSearch}>
+								Поиск
+							</Button>
+						</div>
+					</div>
+					<MovieList />
 				</div>
 			</div>
 		</Curve>
