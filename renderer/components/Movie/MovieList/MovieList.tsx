3b1413f4e60da457e5648114381b0ae698ec08f4
Flame <5017_30@mail.ru>
2024-04-06 15:15:14 +0300

    add: movie skeleton

--- renderer/components/Movie/MovieList/MovieList.tsx
+++ renderer/components/Movie/MovieList/MovieList.tsx
@@ -2,31 +2,56 @@ import { FC, useEffect, useState } from 'react'
 import { Swiper, SwiperSlide } from 'swiper/react'
 import * as Api from '../../../api'
 import { CreateResponseMovieDto } from '../../../api/movie/movie.dto'
+import MovieCardSkeleton from '../../Skeletons/MovieCardSkeleton'
 import MovieCard from '../MovieCard/MovieCard'
 import s from './MovieList.module.scss'
 
 const MovieList: FC = () => {
 	const [movies, setMovies] = useState<CreateResponseMovieDto[]>([])
+	const [isLoading, setIsLoading] = useState(true)
 
 	useEffect(() => {
 		Api.movie.getAll().then(res => {
 			setMovies(res)
+			setIsLoading(false)
 		})
 	}, [])
 
 	return (
 		<div className={s.movieList}>
 			<Swiper grabCursor={true} slidesPerView={'auto'} spaceBetween={10}>
-				{movies.map((item, index) => (
-					<SwiperSlide key={index} className={s.swiperSlide}>
-						<MovieCard
-							backgroundImgUrl={item.posterImage}
-							category='test'
-							id={item.id}
-							title={item.title}
-						/>
-					</SwiperSlide>
-				))}
+				{isLoading ? (
+					<>
+						{movies.map((item, index) => (
+							<SwiperSlide key={index} className={s.swiperSlide}>
+								<MovieCard
+									backgroundImgUrl={item.posterImage}
+									category='test'
+									id={item.id}
+									title={item.title}
+								/>
+							</SwiperSlide>
+						))}
+						{new Array(10).fill(null).map((item, index) => (
+							<SwiperSlide key={index} className={s.swiperSlide}>
+								<MovieCardSkeleton items={1} />
+							</SwiperSlide>
+						))}
+					</>
+				) : (
+					<>
+						{movies.map((item, index) => (
+							<SwiperSlide key={index} className={s.swiperSlide}>
+								<MovieCard
+									backgroundImgUrl={item.posterImage}
+									category='test'
+									id={item.id}
+									title={item.title}
+								/>
+							</SwiperSlide>
+						))}
+					</>
+				)}
 			</Swiper>
 		</div>
 	)
---
+++ renderer/components/Skeletons/MovieCardSkeleton.tsx
@@ -0,0 +1,26 @@
+import { FC } from 'react'
+import Skeleton, { SkeletonTheme } from 'react-loading-skeleton'
+import s from './MovieCardSkeleton.module.scss'
+import 'react-loading-skeleton/dist/skeleton.css'
+
+interface MovieCardSkeletonProps {
+	items: number
+}
+
+const MovieCardSkeleton: FC<MovieCardSkeletonProps> = ({ items }) => {
+	return Array(items)
+		.fill(0)
+		.map((_, index) => (
+			<SkeletonTheme
+				key={index}
+				baseColor='#202020'
+				highlightColor='#444'
+				borderRadius={10}
+			>
+				<Skeleton count={1} width={239} height={382} />
+				<Skeleton count={1} width={239} height={28} className={s.bottom} />
+			</SkeletonTheme>
+		))
+}
+
+export default MovieCardSkeleton
